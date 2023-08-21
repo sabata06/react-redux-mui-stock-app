adb8a89ead867bc6782c7d0fba9e7bce917189e9
Your Name <you@example.com>
2023-08-21 12:22:19 +0300

    forms updated

--- src/hooks/useAuthCall.jsx
+++ src/hooks/useAuthCall.jsx
@@ -1,33 +1,69 @@
-import axios from "axios";
-import { useDispatch } from "react-redux";
-import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice";
-import { useNavigate } from "react-router-dom";
+import axios from "axios"
+import {
+  fetchFail,
+  fetchStart,
+  loginSuccess,
+  logoutSuccess,
+  registerSuccess,
+} from "../features/authSlice"
+
+import { useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
 
 const useAuthCall = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   const navigate = useNavigate()
+
+  const BASE_URL = "https://10001.fullstack.clarusway.com/"
+
   const login = async (userInfo) => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    
-    const BASE_URL = "https://meyymetbaba.pythonanywhere.com/";
-    dispatch(fetchStart());
+    dispatch(fetchStart())
     try {
       const { data } = await axios.post(
         `${BASE_URL}account/auth/login/`,
         userInfo
       )
-
       dispatch(loginSuccess(data))
+      toastSuccessNotify("Login performed")
       navigate("/stock")
-      console.log(data);
-      // return data;
+      console.log(data)
     } catch (error) {
       dispatch(fetchFail())
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
-  return { login };
-};
+  const logout = async () => {
+    dispatch(fetchStart())
+    try {
+      await axios.post(`${BASE_URL}account/auth/logout/`)
+      dispatch(logoutSuccess())
+      toastSuccessNotify("Logout performed")
+      navigate("/")
+    } catch (err) {
+      dispatch(fetchFail())
+      toastErrorNotify("Logout can not be performed")
+    }
+  }
 
-export default useAuthCall;
+  const register = async (userInfo) => {
+    dispatch(fetchStart())
+    try {
+      const { data } = await axios.post(
+        `${BASE_URL}account/register/`,
+        userInfo
+      )
+      dispatch(registerSuccess(data))
+      toastSuccessNotify("Register performed")
+      navigate("/stock")
+    } catch (err) {
+      dispatch(fetchFail())
+      toastErrorNotify("Register can not be performed")
+    }
+  }
+
+  return { login, register, logout }
+}
+
+export default useAuthCall
